66b0a663dfa47af5d4fcbff9c06b73a03c079291
Panji Hanum <panji@digits.id>
2025-12-03 07:31:34 +0700

    Update

--- ecosystem.prod.config.cjs
+++ ecosystem.prod.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: "status-fe-prod",
-      script: "http-server",
+      script: "./node_modules/.bin/http-server",
       args: ["dist", "-p", "9200", "-a", "0.0.0.0"],
       instances: 1,
       autorestart: true,
