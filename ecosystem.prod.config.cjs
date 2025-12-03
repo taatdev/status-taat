121450c7c690846fe8e7ffdc02cd0c2b8ecc995a
Panji Hanum <panji@digits.id>
2025-12-03 07:21:24 +0700

    tes

--- ecosystem.prod.config.cjs
+++ ecosystem.prod.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "status-fe-prod",
       script: "serve",
-      args: "-s dist --listen 9200",
+      args: ["-s", "dist", "-l", "9200"],
       instances: 1,
       autorestart: true,
       watch: false,
