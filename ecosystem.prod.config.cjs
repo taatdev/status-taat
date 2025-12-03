8bda3f04c81a20acdd5099f972a2de0dec980408
Panji Hanum <panji@digits.id>
2025-12-03 07:22:25 +0700

    tes

--- ecosystem.prod.config.cjs
+++ ecosystem.prod.config.cjs
@@ -2,12 +2,11 @@ module.exports = {
   apps: [
     {
       name: "status-fe-prod",
-      script: "serve",
-      args: ["-s", "dist", "-l", "9200"],
+      script: "ws",
+      args: ["--spa", "dist", "--port", "9200"],
       instances: 1,
       autorestart: true,
-      watch: false,
-      env_file: ".env",
+      watch: false
     },
   ],
 };
