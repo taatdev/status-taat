cabe7c50f0bdf6ec014b55a5eeb96b4ab296e802
Panji Hanum <panji@digits.id>
2025-12-04 12:57:16 +0700

    Update

--- src/services.ts
+++ src/services.ts
@@ -6,4 +6,6 @@ export interface IService {
 export const services: IService[] = [
     { name: "Website Utama", url: "https://taat.co.id" },
     { name: "Landing Page", url: "https://lp.taat.co.id" },
+    { name: "Link Page", url: "https://link.taat.co.id" },
+    { name: "Ad Page", url: "https://link.taat.co.id" },
 ];
